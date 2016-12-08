5e5e815ada94b40ce38f74ef9d8091d923d9dd7f
mebao <ke.v.pham@vntrip.vn>
2016-12-08 09:22:12 +0700

    The first - Setup

--- thank.php
+++ thank.php
@@ -22,7 +22,7 @@
     <section class="body-sign">
         <div class="center-sign">
             <div class="tkbn">
-                <a href="http://curcumin.maxmua.com/" class="">
+                <a href="http://nanocurcumin.maxmua.com/" class="">
                     <img src="thank/images/thanks.png" style="max-width:350px;">
                 </a>
             </div>
@@ -32,7 +32,7 @@
                     <p class="tkdes">Bạn đã đăng ký thành công, chúng tôi sẽ liên hệ với bạn trong thời gian sớm nhất.</p>
                     <div class="row">
                         <div class="col-sm-12 btn-end">
-                            <a href="http://curcumin.maxmua.com/" class="btn-lg-x">Trở về trang chủ</a>
+                            <a href="http://nanocurcumin.maxmua.com/" class="btn-lg-x">Trở về trang chủ</a>
                         </div>
                     </div>
                 </div>
